296a6fa6ea4ded828ae26ba77e1604d0bd216259
jose tavarez <newyorkcityjose@gmail.com>
2019-11-14 19:29:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,6 @@
 # out the current state.
 
 def display_board(board)
-  puts board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
   puts board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
   puts board = ["  ", "  ", "  ", "O", "O", "O", "O", "O", "O"]
  end
