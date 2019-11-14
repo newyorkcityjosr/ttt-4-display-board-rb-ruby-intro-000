5095bf3b0fa22921f970bb2e466cd471dfd7b802
jose tavarez <newyorkcityjose@gmail.com>
2019-11-14 19:21:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+
 def display_board(board)
-puts board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+puts board = [" X  ","   ","   ","   ","   ","   ","   ","   ","   "]
 
  end
