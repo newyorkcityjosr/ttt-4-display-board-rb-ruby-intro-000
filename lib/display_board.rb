a231c9df4483a71dd19235adccca2732f5323bb0
jose tavarez <newyorkcityjose@gmail.com>
2019-11-14 19:18:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board(board)
-puts board = [" ","X"," "," "," "," "," "," "," "]
+puts board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 
  end
 end
